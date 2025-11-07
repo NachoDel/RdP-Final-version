754bdcafadfa7198af3d741bd7c095e49d2ccb77
Aldana Pavet García <aldana.pavet.garcia@mi.unc.edu.ar>
2025-11-07 17:31:00 -0300

    pruebas para solucionar la politica con la version antes de ciclicbarrir

--- tp2modelo/src/main/Rdp.java
+++ tp2modelo/src/main/Rdp.java
@@ -10,7 +10,7 @@ public class Rdp {
     public int places = 15;
 
     private final double[][] incidenceMatrix = {
-        // t0  t1  t2   t3  t4  t5  t6  t7  t8  t9  t10 t11
+        //  t0  t1  t2   t3  t4  t5  t6  t7  t8  t9  t10 t11
            {-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1 }, // p0
            {-1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // p1
            { 1, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // p2
